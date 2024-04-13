31f2f591b681556511d0f316b8d6a4dea2ba2860
DasMonitor01 <davidneumann2155@gmail.com>
2024-04-13 15:14:09 +0200

    implemented health node for player

---
+++ player/Health.gd
@@ -0,0 +1,26 @@
+extends Node
+
+@export var _health = 100
+@export var _invincible = false
+
+
+func _on_damage(damage: int):
+	_health = _health - damage
+	
+func signal_damage(damage: int):
+	if _invincible:
+		return
+	_on_damage(damage)
+	
+
+func get_health() -> int:
+	return _health
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
 @export var speed = 90
-@export var health = 100
 @onready var _animation = $AnimatedSprite2D
 
 func get_input():
@@ -15,15 +14,6 @@ func _physics_process(delta):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-func _on_damage(damage: int):
-	health = health - damage
-	
-func signal_damage(damage: int):
-	_on_damage(damage)
-
-func get_health() -> int:
-	return health
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
