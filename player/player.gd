1051969e9615c04f684bd1fe18c8a91fd4e3f863
DasMonitor01 <davidneumann2155@gmail.com>
2024-04-13 19:24:03 +0200

    implemented some basic rolling logic

--- player/player.gd
+++ player/player.gd
@@ -8,6 +8,82 @@ enum AnimationStates {
 	IDLE
 }
 
+enum Direction{
+	UP,
+	UP_RIGHT,
+	RIGHT,
+	DOWN_RIGHT,
+	DOWN,
+	DOWN_LEFT,
+	LEFT,
+	UP_LEFT
+}
+
+@export var speed = 90
+@onready var _animation = $AnimatedSprite2D
+@onready var _health = $Health
+
+var state: AnimationStates = AnimationStates.IDLE
+var rolling = false
+var roll_direction: Direction
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_animation.play("idle")
+
+
+func roll_direction_map() -> StringName:
+	match roll_direction:
+		Direction.UP: return "rolling_up"
+		Direction.DOWN: return "rolling_down"
+		Direction.LEFT | Direction.UP_LEFT | Direction.DOWN_LEFT: return "rolling_left"
+		_: return "rolling_right"
+
+func get_move_direction() -> Direction:
+	if Input.is_action_pressed("up"):
+		if Input.is_action_pressed("right"):
+			return Direction.UP_RIGHT
+		elif Input.is_action_pressed("left"):
+			return Direction.UP_LEFT
+		else:
+			return Direction.UP
+	elif Input.is_action_pressed("down"):
+		if Input.is_action_pressed("right"):
+			return Direction.DOWN_RIGHT
+		elif Input.is_action_pressed("left"):
+			return Direction.DOWN_LEFT
+		else:
+			return Direction.DOWN
+	elif Input.is_action_pressed("right"):
+		return Direction.RIGHT
+	else:
+		return Direction.LEFT
+			
+
+func _manage_roll():
+	rolling = true
+	_animation.stop()
+	_animation.play(roll_direction_map())
+	OS.delay_msec(1000)
+	rolling = false
+	_animation.stop()
+	_animation.play(animationStateMap(state))
+
+
+func roll():
+	roll_direction = get_move_direction()
+	var roll_thread = Thread.new()
+	roll_thread.start(_manage_roll)
+	
+
 func animationStateMap(state: AnimationStates) -> StringName:
 	match state:
 		AnimationStates.LEFT: return "left_walk"
@@ -24,41 +100,21 @@ func actionStateMap(action: StringName) -> AnimationStates:
 		"down": return AnimationStates.DOWN
 		_: return AnimationStates.IDLE
 		
-
-@export var speed = 90
-@onready var _animation = $AnimatedSprite2D
-var state: AnimationStates = AnimationStates.IDLE
-var rolling
-
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
-
-func _physics_process(_delta):
-	get_input()
-	move_and_slide()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	_animation.play("idle")
-
-
 func changeAnimationState(action: StringName):
 	var animation = actionStateMap(action)
 	state = animation
 	_animation.stop()
 	_animation.play(animationStateMap(animation))
 
-
-func roll():
-	pass
-
 func _input(event):
+	if rolling:
+		return
+	elif event.is_action_pressed("roll"):
+		roll()
+		return
 	match state:
 		AnimationStates.IDLE:
-			if event.is_action_pressed("roll"):
-				roll()
-			elif event.is_action_pressed("down"):
+			if event.is_action_pressed("down"):
 				changeAnimationState("down")
 			elif event.is_action_pressed("right"):
 				changeAnimationState("right")
@@ -67,9 +123,7 @@ func _input(event):
 			elif event.is_action_pressed("left"):
 				changeAnimationState("left")
 		AnimationStates.DOWN:
-			if event.is_action_pressed("roll"):
-				roll()
-			elif event.is_action_released("down"):
+			if event.is_action_released("down"):
 				if !Input.is_anything_pressed():
 					changeAnimationState("idle")
 				elif Input.is_action_pressed("right"):
@@ -81,9 +135,7 @@ func _input(event):
 				else:
 					changeAnimationState("idle")
 		AnimationStates.RIGHT:
-			if event.is_action_pressed("roll"):
-				roll()
-			elif event.is_action_pressed("down"):
+			if event.is_action_pressed("down"):
 				changeAnimationState("down")
 			elif event.is_action_released("right"):
 				if !Input.is_anything_pressed():
@@ -95,9 +147,7 @@ func _input(event):
 				else:
 					changeAnimationState("idle")
 		AnimationStates.UP:
-			if event.is_action_pressed("roll"):
-				roll()
-			elif event.is_action_pressed("down"):
+			if event.is_action_pressed("down"):
 				changeAnimationState("down")
 			elif event.is_action_pressed("right"):
 				changeAnimationState("right")
@@ -109,9 +159,7 @@ func _input(event):
 				else:
 					changeAnimationState("idle")
 		AnimationStates.LEFT:
-			if event.is_action_pressed("roll"):
-				roll()
-			elif event.is_action("down"):
+			if event.is_action("down"):
 				changeAnimationState("down")
 			elif event.is_action_pressed("right"):
 				changeAnimationState("right")
