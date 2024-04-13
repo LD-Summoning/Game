b40296b0d0deb91679268e3f4de0e074bd660138
DasMonitor01 <davidneumann2155@gmail.com>
2024-04-13 15:19:34 +0200

    ?

--- player/player.gd
+++ player/player.gd
@@ -7,7 +7,7 @@ func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	get_input()
 	move_and_slide()
 
@@ -16,7 +16,7 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("left"):
 		_animation.stop()
 		_animation.play("left_walk")
