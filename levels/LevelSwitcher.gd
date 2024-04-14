2a1bdf97718094e1d73af57a3de573b5d016d9f3
WeiiswurstDev <arved.ehrnsperger@gmail.com>
2024-04-14 12:19:56 +0200

    fix ice imp, add enemies

--- levels/LevelSwitcher.gd
+++ levels/LevelSwitcher.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var level: Node
+var new_level_scene: PackedScene
 @onready var timer = $LevelSwitchCooldown
 var can_switch = true
 
@@ -8,16 +9,21 @@ func _ready():
 	level = get_parent().get_node("Level")
 
 
-func _on_level_scene_change(new_scene: PackedScene):
-	if can_switch:
+func _process(_delta):
+	if can_switch && new_level_scene != null:
 		can_switch = false
-		var new_level = new_scene.instantiate()
+		var new_level = new_level_scene.instantiate()
 		new_level.connect("scene_change", _on_level_scene_change)
 		get_parent().add_child(new_level)
 		get_parent().move_child(new_level, 0)
 		level.queue_free()
 		level = new_level
 		timer.start()
+		new_level_scene = null
+
+
+func _on_level_scene_change(new_scene: PackedScene):
+	new_level_scene = new_scene
 
 
 func _on_level_switch_cooldown_timeout():
