eb4955fba1999cb948faad146efe77cfb4a38439
WeiiswurstDev <arved.ehrnsperger@gmail.com>
2024-04-16 00:47:27 +0200

    add pause screen

---
+++ common/pause_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = !get_tree().paused
+	if get_tree().paused:
+		show()
+	else:
+		hide()
+
